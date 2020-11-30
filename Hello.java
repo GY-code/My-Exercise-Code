cfe734446face71f7b398ae595d3119786ab9da1
GY-code <64950646+GY-code@users.noreply.github.com>
2020-11-30 16:14:25 +0800

    star

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 //Java is known as OOP
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is the hello from ¹¼Ò× 19301032.");
+		System.out.println("This is the hello from ** *******2.");
 		System.out.println("This is the second line : Damn glad to meet you in Java Class!");
 	}
 }
