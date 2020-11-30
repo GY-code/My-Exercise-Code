b9fff193e23234209f1c3b5b0c04b2e67ca1e8f3
GuYi <gu_yi163@163.com>
2020-11-30 16:05:19 +0800

    This is a hello world code in Java.

---
+++ Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the hello from ¹¼Ò× 19301032.");
+		System.out.println("This is the second line : Damn glad to meet you in Java Class!");
+	}
+}
