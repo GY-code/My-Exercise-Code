ee80d0a702fb94183bd4d69f4edeb46e9e95d0e4
GY-code <64950646+GY-code@users.noreply.github.com>
2020-11-30 16:12:38 +0800

    OOP comment

--- Hello.java
+++ Hello.java
@@ -1,9 +1,10 @@
 
 public class Hello {
-
+//Java is known as OOP
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is the hello from ¹¼Ò× 19301032.");
+		System.out.println("This is the hello from Â¹Â¼Ã’Ã— 19301032.");
 		System.out.println("This is the second line : Damn glad to meet you in Java Class!");
 	}
 }
+
