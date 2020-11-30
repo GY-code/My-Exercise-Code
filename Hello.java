0001b0e992f6f8bd2d1168976d3bdced8ea571a5
GuYi <gu_yi163@163.com>
2020-11-30 16:22:00 +0800

    a test of merge conflict resolution

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is the hello from ** *******2.");
+		//this is my personal information which should be removed
 		System.out.println("This is the second line : Damn glad to meet you in Java Class!");
 	}
 }
